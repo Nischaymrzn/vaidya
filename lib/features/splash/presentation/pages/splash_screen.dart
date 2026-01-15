d4f10a887ceaaa09b62f79f9dd42de5280f2f339
Nischaymrzn <nischaymaharjann@gmail.com>
2026-01-15 07:53:27 +0545

    feat: save user session in shared pref

--- lib/features/splash/presentation/pages/splash_screen.dart
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -1,26 +1,41 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vaidya/app/routes/app_routes.dart';
+import 'package:vaidya/core/services/storage/user_session_service.dart';
 import 'package:vaidya/core/widgets/loader.dart';
+import 'package:vaidya/features/dashboard/presentation/pages/dashboard.dart';
 import 'package:vaidya/features/onboarding/presentation/pages/onboarding_screen.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    _navigateToNext();
+  }
 
-    Timer(const Duration(seconds: 5), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => OnboardingScreen()),
-      );
-    });
+  Future<void> _navigateToNext() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+
+    // Check if user is already logged in
+    final userSessionService = ref.read(userSessionServiceProvider);
+    final isLoggedIn = userSessionService.isLoggedIn();
+
+    if (isLoggedIn) {
+      // Navigate to Dashboard if user is logged in
+      AppRoutes.pushReplacement(context, const DashboardScreen());
+    } else {
+      // Navigate to Onboarding if user is not logged in
+      AppRoutes.pushReplacement(context, const OnboardingScreen());
+    }
   }
 
   @override
