52d26033f44faf7f8fee7d68c2d87a8cfbe05588
Nischaymrzn <nischaymaharjann@gmail.com>
2026-01-15 18:29:35 +0545

    fix: logout implementation in profile screen

--- lib/features/dashboard/presentation/pages/profile.dart
+++ lib/features/dashboard/presentation/pages/profile.dart
@@ -1,16 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vaidya/app/routes/app_routes.dart';
+import 'package:vaidya/features/auth/presentation/pages/login_screen.dart';
+import 'package:vaidya/features/auth/presentation/view_model/auth_viewmodel.dart';
+import 'package:vaidya/themes/colors.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return SizedBox.expand(
       child: Center(
-        child: Text(
-          "This is Profile Page",
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "This is Profile Page",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 20),
+
+            ElevatedButton.icon(
+              icon: const Icon(Icons.logout),
+              label: const Text("Logout"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primary,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: () => _showLogoutDialog(context, ref),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  void _showLogoutDialog(BuildContext context, WidgetRef ref) {
+    Future<void> handleLogout() async {
+      AppRoutes.pop(context); // close dialog
+
+      await ref.read(authViewModelProvider.notifier).logout();
+
+      if (context.mounted) {
+        AppRoutes.pushAndRemoveUntil(context, const LoginScreen());
+      }
+    }
+
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        title: const Text(
+          "Logout",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        content: const Text("Are you sure you want to logout?"),
+        actions: [
+          TextButton(
+            onPressed: () => AppRoutes.pop(context),
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: handleLogout,
+            child: const Text(
+              "Logout",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
       ),
     );
   }
