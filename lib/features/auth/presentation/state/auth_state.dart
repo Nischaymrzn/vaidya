80d55675e43dbd7cbd74758534ac6d594b242230
Nischaymrzn <nischaymaharjann@gmail.com>
2026-01-30 10:12:22 +0545

    feat: refactored auth state code

--- lib/features/auth/presentation/state/auth_state.dart
+++ lib/features/auth/presentation/state/auth_state.dart
@@ -14,25 +14,29 @@ class AuthState extends Equatable {
   final AuthStatus status;
   final AuthEntity? user;
   final String? errorMessage;
+  final String? successMessage;
 
   const AuthState({
     this.status = AuthStatus.initial,
     this.user,
     this.errorMessage,
+    this.successMessage,
   });
 
   AuthState copyWith({
     AuthStatus? status,
     AuthEntity? user,
     String? errorMessage,
+    String? successMessage,
   }) {
     return AuthState(
       status: status ?? this.status,
       user: user ?? this.user,
       errorMessage: errorMessage ?? this.errorMessage,
+      successMessage: successMessage ?? this.successMessage,
     );
   }
 
   @override
-  List<Object?> get props => [status, user, errorMessage];
+  List<Object?> get props => [status, user, errorMessage, successMessage];
 }
