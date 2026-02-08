24831067d3c1c51d09a5e1ce5d00e7a7169f2f40
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-08 15:36:00 +0545

    fix: update health suggestion

--- lib/features/dashboard/presentation/widgets/health_suggestion.dart
+++ lib/features/dashboard/presentation/widgets/health_suggestion.dart
@@ -3,9 +3,8 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vaidya/themes/colors.dart';
 
 class HealthSuggestion extends StatelessWidget {
-  final dynamic title;
-
-  final dynamic desc;
+  final String title;
+  final String desc;
 
   const HealthSuggestion({super.key, required this.title, required this.desc});
 
@@ -13,45 +12,41 @@ class HealthSuggestion extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: EdgeInsets.all(16),
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: Color(0xFFE5F0FF),
+        color: const Color(0xFFE5F0FF),
         borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-          color: AppColors.border,
-          width: 1, // thin border
-        ),
+        border: Border.all(color: AppColors.border, width: 1),
       ),
       child: Column(
         children: [
           Row(
             children: [
-              SvgPicture.asset("assets/icons/suggestion.svg", width: 18),
+              SvgPicture.asset('assets/icons/suggestion.svg', width: 18),
               const SizedBox(width: 8),
               const Text(
-                "Health Suggestion",
+                'Health Suggestion',
                 style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
                   color: Color(0xFF1E3A8A),
                 ),
               ),
             ],
           ),
           const SizedBox(height: 12),
-
           Row(
             children: [
               Container(
                 padding: const EdgeInsets.all(12),
                 alignment: Alignment.center,
                 decoration: BoxDecoration(
-                  color: Color(0xFFF2F8FF),
+                  color: const Color(0xFFF2F8FF),
                   shape: BoxShape.circle,
-                  border: Border.all(color: Color(0XFFE5E7EB), width: 2),
+                  border: Border.all(color: const Color(0xFFE5E7EB), width: 2),
                 ),
                 child: SvgPicture.asset(
-                  "assets/icons/blood_pressure.svg",
+                  'assets/icons/blood_pressure.svg',
                   height: 20,
                   width: 20,
                 ),
@@ -63,18 +58,19 @@ class HealthSuggestion extends StatelessWidget {
                   children: [
                     Text(
                       title,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
                         color: AppColors.textPrimary,
                       ),
                     ),
                     Text(
                       desc,
                       softWrap: true,
-                      style: TextStyle(
-                        fontSize: 15,
+                      style: const TextStyle(
+                        fontSize: 14,
                         color: AppColors.textSecondary,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ],
