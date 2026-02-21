8cf831c3da9b7798ebae2b51ede02072948b41ca
Nischaymrzn <nischaymaharjann@gmail.com>
2026-02-21 09:44:56 +0545

    feat: update profile user api model

--- lib/features/profile/data/models/profile_user_api_model.dart
+++ lib/features/profile/data/models/profile_user_api_model.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:vaidya/features/profile/domain/entities/profile_user_entity.dart';
 
 class ProfileUserApiModel {
@@ -15,4 +16,20 @@ class ProfileUserApiModel {
   }
 
   ProfileUserEntity toEntity() => ProfileUserEntity(id: id, data: data);
+
+  static Future<FormData> buildUpdateFormData(
+    Map<String, dynamic> payload, {
+    String? imagePath,
+  }) async {
+    final map = Map<String, dynamic>.from(payload);
+    final trimmedPath = imagePath?.trim();
+    if (trimmedPath != null && trimmedPath.isNotEmpty) {
+      final filename = trimmedPath.split(RegExp(r'[/\\]')).last;
+      map['image'] = await MultipartFile.fromFile(
+        trimmedPath,
+        filename: filename,
+      );
+    }
+    return FormData.fromMap(map);
+  }
 }
