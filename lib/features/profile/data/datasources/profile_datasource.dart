8f833ff59cd3e348e535b1afaecac7aaaf3e9657
Nischaymrzn <nischaymaharjann@gmail.com>
2026-03-03 09:47:00 +0545

    fix: update profile local datasource

--- lib/features/profile/data/datasources/profile_datasource.dart
+++ lib/features/profile/data/datasources/profile_datasource.dart
@@ -1,12 +1,22 @@
 import 'package:vaidya/features/profile/data/models/profile_user_api_model.dart';
+import 'package:vaidya/features/profile/data/models/profile_payment_status_api_model.dart';
+import 'package:vaidya/features/profile/data/models/profile_checkout_session_api_model.dart';
 
 abstract interface class IProfileRemoteDataSource {
   Future<ProfileUserApiModel> getUserById(String id);
-  Future<ProfileUserApiModel> updateUser(String id, Map<String, dynamic> payload, {String? imagePath});
+  Future<ProfileUserApiModel> updateUser(
+    String id,
+    Map<String, dynamic> payload, {
+    String? imagePath,
+  });
   Future<void> deleteUser(String id);
+  Future<ProfilePaymentStatusApiModel> getPaymentStatus();
+  Future<ProfileCheckoutSessionApiModel> createCheckoutSession();
 }
 
 abstract interface class IProfileLocalDataSource {
-  Future<void> cacheUser(ProfileUserApiModel user);
-  Future<ProfileUserApiModel?> getCachedUser();
+  Future<void> saveUser(ProfileUserApiModel user);
+  Future<ProfileUserApiModel?> getUser();
+  Future<void> savePaymentStatus(ProfilePaymentStatusApiModel status);
+  Future<ProfilePaymentStatusApiModel?> getPaymentStatus();
 }
